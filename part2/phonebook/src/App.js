1c9dc3264e383a46f54cd836f447537b1f9ea4e5
Henry Lin <henry.lin.hln@gmail.com>
2020-06-25 16:44:13 -0400

    Exercise 2.8 completed
    Added functionality to add phone numbers to phone book

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,35 +2,52 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
-  const addName = (event) => {
+  const addPerson = (event) => {
       event.preventDefault()
-      console.log(newName)
+
       if (persons.some(person => person.name === newName)) {
         window.alert(`${newName} is already added to phonebook`)
       } else {
         const nameObject = {
-            name: newName
+            name: newName,
+            number: newNumber
         }
         setPersons(persons.concat(nameObject));
-        setNewName('') 
+        setNewName('')
+        setNewNumber('')
       }
   }
+  
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input
             value={newName}
             onChange={handleNameChange}
+           />
+        </div>
+        <div>
+          number: <input
+            value={newNumber}
+            onChange={handleNumberChange}
            />
         </div>
         <div>
@@ -47,7 +64,11 @@ const App = () => {
   )
 }
 
-const Person = ({person}) => <div>{person.name}</div>
+const Person = ({person}) => {
+    return (
+        <div>{person.name} {person.number}</div>
+    )
+}
 
 
 export default App
