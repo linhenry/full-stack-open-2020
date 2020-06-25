788e8fd0214b5d0a2c4f632788a07a863881fae2
Henry Lin <henry.lin.hln@gmail.com>
2020-06-25 00:17:50 -0400

    Exercise 2.5 completed
    Divided Course component as a separate module

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const totalExercises = course.parts.reduce((sum, part) => {
+    return sum + part.exercises
+  }, 0)
+
+  return (
+    <p><strong>total of {totalExercises} exercises</strong></p>
+  ) 
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(part =>
+        <Part key={part.id} part={part} />
+      )}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+    return (
+      <div>
+        <Header course={course} />
+        <Content course={course} />
+        <Total course={course} />
+      </div>
+    )
+  }
+
+export default Course
