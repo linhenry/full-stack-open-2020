90b766c47d29929aafd66194a4334b97d125a578
Henry Lin <henry.lin.hln@gmail.com>
2020-06-25 21:34:52 -0400

    Exercise 2.11 completed
    Added json-server and axios dependencies and modified phonebook to grab from json-server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
+    // const [persons, setPersons] = useState([
+    //     { name: 'Arto Hellas', number: '040-123456' },
+    //     { name: 'Ada Lovelace', number: '39-44-5323523' },
+    //     { name: 'Dan Abramov', number: '12-43-234345' },
+    //     { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    // ])
+    const [persons, setPersons] = useState([])
     const [filterName, setFilterName] = useState('')
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data)
+            })
+    }, [])
+    // using [] ensures that effect is only run along with first render of component
 
     const addPerson = (event) => {
         event.preventDefault()
