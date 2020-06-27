430826be035e602669158e9c8c75920fc5c199d4
Henry Lin <henry.lin.hln@gmail.com>
2020-06-26 20:20:37 -0400

    Exercise 2.14 completed
    Added API from weatherstack.com to display weather in selected country

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 const Country = ({country}) => {
+    const [weather, setWeather] = useState(null)
+
+    useEffect(() => {
+        axios
+            .get(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${country.name}`)
+            .then(response => {
+                setWeather(response.data.current)
+            })
+    }, [country])
+
     return (
         <div>
             <h1>{country.name}</h1>
@@ -15,6 +26,18 @@ const Country = ({country}) => {
             </div>
             <div>
                 <img src={country.flag} alt="Selected country's flag" width="150" height="150"/>
+            </div>
+            <h2>weather in {country.capital}</h2>
+            <div>
+                {
+                weather ?
+                <div>
+                    <div><strong>temperature: </strong>{weather.temperature} Celcius</div>
+                    <img src={weather.weather_icons} alt="Weather icon" width="150" height="150"/>
+                    <div><strong>wind: </strong> {weather.wind_speed} mph, direction {weather.wind_dir}</div>
+                </div> :
+                <div>loading...</div>
+                }
             </div>
         </div>
     );
