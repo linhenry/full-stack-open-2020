2ff46b3d95b0d0cb79dd7f64c6eba2f8cdc6a0cf
Henry Lin <henry.lin.hln@gmail.com>
2020-06-27 21:09:00 -0400

    Exercises 2.15 and 2.16 completed
    Added person service to handle REST API requests

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,29 +1,22 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/person'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-    // const [persons, setPersons] = useState([
-    //     { name: 'Arto Hellas', number: '040-123456' },
-    //     { name: 'Ada Lovelace', number: '39-44-5323523' },
-    //     { name: 'Dan Abramov', number: '12-43-234345' },
-    //     { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    // ])
     const [persons, setPersons] = useState([])
     const [filterName, setFilterName] = useState('')
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
 
     useEffect(() => {
-        axios
-            .get('http://localhost:3001/persons')
-            .then(response => {
-                setPersons(response.data)
+        personService
+            .getAll()
+            .then(initialPersons => {
+                setPersons(initialPersons)
             })
     }, [])
-    // using [] ensures that effect is only run along with first render of component
 
     const addPerson = (event) => {
         event.preventDefault()
@@ -31,13 +24,17 @@ const App = () => {
         if (persons.some(person => person.name === newName)) {
             window.alert(`${newName} is already added to phonebook`)
         } else {
-            const nameObject = {
+            const personObject = {
                 name: newName,
                 number: newNumber
             }
-            setPersons(persons.concat(nameObject));
-            setNewName('')
-            setNewNumber('')
+            personService
+                .create(personObject)
+                .then(createdPerson => {
+                    setPersons(persons.concat(createdPerson))
+                    setNewName('')
+                    setNewNumber('')
+                })
         }
     }
 
@@ -50,15 +47,10 @@ const App = () => {
     }
 
     const handleFilterNameChange = (event) => {
-        console.log(filterName)
         setFilterName(event.target.value)
     }
 
-    const filteredPersons = persons.filter(person => {
-        return (
-            filterName, person.name.toLowerCase().includes(filterName.toLowerCase())
-        )
-    })
+    const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(filterName.toLowerCase()))
 
     return (
         <div>
